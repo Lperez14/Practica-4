a186378de1741d7bfe35946a13fcb41134dc94cc
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-02-17 18:46:51 -0400

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa3
+{
+    class Ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            /*Ejercicio 3*/
+            int  primeros= 1;
+
+            for (int numero = 100; primeros <= 10 ; numero--)
+            {
+                if(numero % 2 == 0)
+                {
+                    Console.WriteLine( primeros + "- "+numero);
+                    primeros++;
+                        
+                } 
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
