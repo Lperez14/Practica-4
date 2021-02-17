0ada313df935250a786c326b4ee9d6fad1430685
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-02-17 18:47:21 -0400

    Add files via upload

---
+++ Ejericio2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Ejericio2
+    {
+        static void Main(string[] args)
+        {
+            /*Ejercicio 2*/
+            int numero=1;
+
+            do
+            {
+                Console.WriteLine("Numero: " + numero);
+                numero++;
+             
+             
+            }
+            while (numero <= 10);
+
+            Console.ReadKey();
+        }
+    }
+}
