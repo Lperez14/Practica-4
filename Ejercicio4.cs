207afb93912301ba0bacd143352dc6b48270ef9f
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-02-17 18:47:08 -0400

    Add files via upload

---
+++ Ejercicio4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+
+            /**/
+
+            char abecedario = 'A';
+            
+            do
+            {
+                Console.WriteLine(abecedario);
+                abecedario++;
+
+            }
+            while (abecedario <= 'Z');
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
