c4c70d9be372c1296b8dc11fd686223713a97024
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-02-17 18:46:37 -0400

    Add files via upload

---
+++ Ejercicio1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ejercicio 1*/
+            int numero = 1,total = 0;
+            do
+            {
+                Console.Write("Digite un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if(numero > 0)
+                {
+                    total += numero;
+                } 
+               
+                
+            } while (numero >= 1);
+
+            Console.WriteLine("La suma total de los numero introducidos es: " + total);
+
+            Console.ReadKey();
+        }
+    }
+}
